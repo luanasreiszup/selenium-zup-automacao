162991daeae0bfdb9428274a1586787c2806b975
ayrtonNeto <ayrton.neto@zup.com.br>
2022-02-01 18:28:20 -0300

    Fale conoscon

--- zup-automation-selenium/src/main/java/core/util/massaDados/FaleDados.java
+++ zup-automation-selenium/src/main/java/core/util/massaDados/FaleDados.java
@@ -1,5 +1,37 @@
 package core.util.massaDados;
 
-public class FaleDados {
+import core.util.leitorJson.LeitorJson;
 
+public class FaleDados {
+	
+	private String nome;
+	private String email;
+	private String assunto;
+	private String mensagem;
+	private String notificacao;
+
+	public String getNome() {
+		return nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public String getAssunto() {
+		return assunto;
+	}
+	public String getMensagem() {
+		return mensagem;
+	}
+	
+	public String getNotificacao() {
+		return notificacao;
+	}
+	
+	public FaleDados(String json_file, String id_massa) {
+		this.nome = LeitorJson.PesquisaJson(json_file, id_massa, "nome");
+		this.email = LeitorJson.PesquisaJson(json_file, id_massa, "email");
+		this.assunto = LeitorJson.PesquisaJson(json_file, id_massa, "assunto");
+		this.mensagem = LeitorJson.PesquisaJson(json_file, id_massa, "mensagem");
+		this.notificacao = LeitorJson.PesquisaJson(json_file, id_massa, "noticacao");
+	}
 }
